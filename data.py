5e49fd60e3c29b615ca14eb5a52b7e8a12891b1e
Aleksandr <belskikh.aleksandr@gmail.com>
2018-08-13 10:06:48 +0300

    data structure

---
+++ data.py
@@ -0,0 +1,23 @@
+from torch.utils.data import DataLoader, Dataset
+
+
+class DataKek(Dataset):
+    def __init__(self):
+        pass
+
+    def __getitem__(self, item):
+        pass
+
+    def __len__(self):
+        pass
+
+
+class DataKeker(DataLoader):
+    def __init__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __len__(self):
+        pass
