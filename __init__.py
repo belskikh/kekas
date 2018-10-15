833ea743ed17e169e5081b15e00edc446200c9ee
Aleksandr <belskikh.aleksandr@gmail.com>
2018-10-15 14:55:38 +0300

    DataOwner and Tranformer classes

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from .keker import Keker
-from .data import DataKek
+from .data import DataKek, DataOwner
+from .transformer import Transformer
 
 from . import modules
--- data.py
+++ data.py
@@ -2,26 +2,27 @@ from torch.utils.data import DataLoader, Dataset
 
 
 class DataKek(Dataset):
-    """The easiest way to store the dataset info is pandas dataframe. So DataKek gets a dataframe as src of data"""
-    def __init__(self, df, transforms=None):
+    """The easiest way to store the dataset info is pandas DataFrame.
+    So DataKek gets a DataFrame as a source of data"""
+    def __init__(self, df, reader_fn=None, transforms=None):
         # transform df to list
-        self.data = [row for i, row in df.iterrows()]
+        self.data = list(df.iterrows())  # for multiprocessing
+        self.reader_fn = reader_fn
         self.transforms = transforms
-        pass
 
-    def __getitem__(self, item):
-        pass
+    def __getitem__(self, ind):
+        datum = self.reader_fn(*self.data[ind])
+        if self.transforms is not None:
+            datum = self.transforms(datum)
+        return datum
 
     def __len__(self):
         return len(self.data)
 
 
-# class DataKeker(DataLoader):
-#     def __init__(self, dataset):
-#         super().__init__(dataset)
-#
-#     # def __iter__(self):
-#     #     pass
-#     #
-#     # def __len__(self):
-#     #     pass
+class DataOwner:
+    def __init__(self, trn_dl, val_dl, tst_dl=None):
+        # TODO: protect theese things from overwriting
+        self.trn_dl = trn_dl
+        self.val_dl = val_dl
+        self.tst_dl = tst_dl
