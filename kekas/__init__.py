ac62d24c86295165abd290994852cb00d40ebe01
Aleksandr <belskikh.aleksandr@gmail.com>
2019-02-15 14:50:39 +0300

    version

--- kekas/__init__.py
+++ kekas/__init__.py
@@ -3,3 +3,5 @@ from .data import DataKek, DataOwner
 from .transformations import Transformer, to_torch, normalize
 
 from . import modules
+
+__version__ = "0.1.5"
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kekas",
-    version="0.1.4",
+    version="0.1.5",
     author="Aleksandr Belskikh",
     author_email="belskikh.aleksandr@gmail.com",
     description="Jast another DL library.",
