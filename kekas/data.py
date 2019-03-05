2689c912658acf74439153f2d6a1f9fdd91506e1
Aleksandr <belskikh.aleksandr@gmail.com>
2019-03-05 11:36:39 +0300

    fast dataframe listify

--- kekas/data.py
+++ kekas/data.py
@@ -5,6 +5,7 @@ import pandas as pd
 from torch.utils.data import Dataset
 from torchvision.transforms import Compose
 
+
 class DataKek(Dataset):
     """The easiest way to store the dataset info is pandas DataFrame.
     So DataKek gets a DataFrame as a source of data"""
@@ -13,12 +14,14 @@ class DataKek(Dataset):
                  reader_fn: Callable,
                  transforms: Optional[Compose] = None) -> None:
         # transform df to list
-        self.data = list(df.iterrows())  # for multiprocessing
+        self.data = list(df.values)  # for multiprocessing
+        self.df_columns = df.columns
         self.reader_fn = reader_fn
         self.transforms = transforms
 
     def __getitem__(self, ind: int) -> Dict:
-        datum = self.reader_fn(*self.data[ind])
+        data_dict = {col: v for col, v in zip(self.df_columns, self.data[ind])}
+        datum = self.reader_fn(ind, data_dict)
         if self.transforms is not None:
             datum = self.transforms(datum)
         return datum
